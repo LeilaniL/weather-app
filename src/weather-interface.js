137b8876d886137c54b92d2b43ec1eb2507942ec
gulzat karimova, leilani leach and stuart gill <leilani@>
2019-01-15 08:57:46 -0800

    update to move jQuery to main.js

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,5 +1,5 @@
-// AJAX jQuery version below
-
+// // AJAX jQuery version below
+//
 // $(document).ready(function () {
 //   $('#weatherLocation').click(function () {
 //     let city = $('#location').val();
@@ -20,29 +20,29 @@
 //     });
 //   });
 // });
-// XMLHttpRequest version below
-
-$(document).ready(function () {
-  $('#weatherLocation').click(function () {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
-
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-    const getElements = function (response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
-  });
-});
+// // XMLHttpRequest version below
+//
+// // $(document).ready(function () {
+// //   $('#weatherLocation').click(function () {
+// //     const city = $('#location').val();
+// //     $('#location').val("");
+// //
+// //     let request = new XMLHttpRequest();
+// //     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[APPKEY]`;
+// //
+// //     request.onreadystatechange = function () {
+// //       if (this.readyState === 4 && this.status === 200) {
+// //         const response = JSON.parse(this.responseText);
+// //         getElements(response);
+// //       }
+// //     }
+// //
+// //     request.open("GET", url, true);
+// //     request.send();
+// //
+// //     const getElements = function (response) {
+// //       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+// //       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+// //     }
+// //   });
+// // });
